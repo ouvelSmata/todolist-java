d3a01bd32b3fbc427486093739080bc3083abdac
Ouvel Smata <ouvelsmata@gmail.com>
2023-01-14 02:23:07 +0000

    add model

---
+++ src/main/java/kimha/todolist/TodolistApp.java
@@ -0,0 +1,13 @@
+package kimha.todolist;
+
+/**
+ * TodolistApp
+ */
+public class TodolistApp {
+
+  private String[] model = new String[10];
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
