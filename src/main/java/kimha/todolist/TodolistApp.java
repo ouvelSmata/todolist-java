2dde02a6c545e29b01a9c198ebcc5ee03ae2c2e8
Ouvel Smata <ouvelsmata@gmail.com>
2023-01-14 20:37:18 +0000

    add business logic

--- src/main/java/kimha/todolist/TodolistApp.java
+++ src/main/java/kimha/todolist/TodolistApp.java
@@ -10,4 +10,16 @@ public class TodolistApp {
   public static void main(String[] args) {
     System.out.println("Hello World");
   }
+
+  // Show Todolist
+  public static void showTodolist() {
+  }
+
+  // Add Todolist
+  public static void addTodolist() {
+  }
+
+  // Remove Todolist
+  public static void removeTodolist() {
+  }
 }
