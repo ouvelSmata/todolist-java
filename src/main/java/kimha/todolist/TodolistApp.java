b668ff2007da20397010a4524bef843a6620c5db
Ouvel Smata <ouvelsmata@gmail.com>
2023-01-16 03:31:59 +0000

    add show todo

--- src/main/java/kimha/todolist/TodolistApp.java
+++ src/main/java/kimha/todolist/TodolistApp.java
@@ -5,7 +5,7 @@ package kimha.todolist;
  */
 public class TodolistApp {
 
-  private String[] model = new String[10];
+  private static String[] model = new String[10];
 
   public static void main(String[] args) {
     System.out.println("Hello World");
@@ -13,6 +13,14 @@ public class TodolistApp {
 
   // Show Todolist
   public static void showTodolist() {
+    for (int i = 0; i < model.length; i++) {
+      String todo = model[i];
+      int index = i + 1;
+
+      if (todo != null) {
+        System.out.println(index + ". " + todo);
+      }
+    }
   }
 
   // Add Todolist
