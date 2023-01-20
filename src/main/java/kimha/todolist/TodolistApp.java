2eee84e143c5ed0e6206ad49c8e6324be03e009b
Ouvel Smata <ouvelsmata@gmail.com>
2023-01-20 21:01:20 +0000

    wrapup all

--- src/main/java/kimha/todolist/TodolistApp.java
+++ src/main/java/kimha/todolist/TodolistApp.java
@@ -10,7 +10,7 @@ public class TodolistApp {
   private static java.util.Scanner scanner = new java.util.Scanner(System.in);
 
   public static void main(String[] args) {
-    testViewRemoveTodolist();
+    viewShowTodolist();
   }
 
   // Show Todolist
