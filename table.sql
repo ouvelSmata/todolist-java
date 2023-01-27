7f9cf312f81bc9d477b8b6c4b77dd5bb6870aebb
Ouvel Smata <ouvelsmata@gmail.com>
2023-01-27 02:53:20 +0000

    add table

---
+++ table.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE mysql_todolist;
+
+USE mysql_todolist;
+
+SHOW TABLES;
+
+CREATE TABLE todolist (
+  id INT NOT NULL AUTO_INCREMENT,
+  todo VARCHAR(255) NOT NULL,
+  PRIMARY KEY(id)
+)ENGINE = InnoDB;
+
+DESC todolist;
