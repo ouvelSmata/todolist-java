d45016afea4ad198205c685a79d7a620b46d1bed
Ouvel Smata <ouvelsmata@gmail.com>
2023-02-04 03:25:40 +0000

    add repository remove

--- src/main/java/kimha/todolist/repository/TodolistRepositoryImpl.java
+++ src/main/java/kimha/todolist/repository/TodolistRepositoryImpl.java
@@ -2,6 +2,7 @@ package kimha.todolist.repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
@@ -26,28 +27,28 @@ public class TodolistRepositoryImpl implements TodolistRepository {
     return data;
   }
 
-  private boolean isFull() {
-    boolean isFull = true;
+  // private boolean isFull() {
+  // boolean isFull = true;
 
-    for (int i = 0; i < data.length; i++) {
-      if (data[i] != null) {
-        isFull = false;
-      }
-    }
+  // for (int i = 0; i < data.length; i++) {
+  // if (data[i] != null) {
+  // isFull = false;
+  // }
+  // }
 
-    return isFull;
-  }
+  // return isFull;
+  // }
 
-  private void resizeIfFull() {
-    if (isFull()) {
-      Todolist[] temp = data;
-      data = new Todolist[data.length * 2];
+  // private void resizeIfFull() {
+  // if (isFull()) {
+  // Todolist[] temp = data;
+  // data = new Todolist[data.length * 2];
 
-      for (int i = 0; i < temp.length; i++) {
-        data[i] = temp[i];
-      }
-    }
-  }
+  // for (int i = 0; i < temp.length; i++) {
+  // data[i] = temp[i];
+  // }
+  // }
+  // }
 
   @Override
   public void add(Todolist todolist) {
@@ -71,21 +72,54 @@ public class TodolistRepositoryImpl implements TodolistRepository {
     }
   }
 
-  @Override
-  public boolean remove(Integer number) {
-    if ((number - 1) >= data.length) {
-      return false;
-    } else if (data[number - 1] == null) {
-      return false;
-    } else {
-      for (int i = (number - 1); i < data.length; i++) {
-        if (i == data.length - 1) {
-          data[i] = null;
+  public boolean isExist(Integer number) {
+    String sql = "SELECT * FROM todolist WHERE id = ?";
+
+    try (Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);) {
+      statement.setInt(1, number);
+
+      try (ResultSet resultSet = statement.executeQuery()) {
+        if (resultSet.next()) {
+          return true;
         } else {
-          data[i] = data[i + 1];
+          return false;
         }
       }
+    } catch (SQLException exception) {
+      throw new RuntimeException(exception);
+    }
+  }
+
+  @Override
+  public boolean remove(Integer number) {
+    // if ((number - 1) >= data.length) {
+    // return false;
+    // } else if (data[number - 1] == null) {
+    // return false;
+    // } else {
+    // for (int i = (number - 1); i < data.length; i++) {
+    // if (i == data.length - 1) {
+    // data[i] = null;
+    // } else {
+    // data[i] = data[i + 1];
+    // }
+    // }
+    // return true;
+    // }
+    String sql = "DELETE FROM todolist WHERE id = ?";
+
+    if (isExist(number)) {
+      try (Connection connection = dataSource.getConnection();
+          PreparedStatement statement = connection.prepareStatement(sql)) {
+        statement.setInt(1, number);
+        statement.executeUpdate();
+      } catch (SQLException e) {
+        throw new RuntimeException(e);
+      }
       return true;
+    } else {
+      return false;
     }
   }
 
